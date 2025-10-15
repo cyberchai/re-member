501dfba336bd11d2bbe293ce116de57bffb42667
Math Random <math.random0@gmail.com>
2025-10-15 02:49:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-862965224-88487",
+  "appId": "1:639788228039:web:129b0b2ac223d3ef87cde2",
+  "apiKey": "AIzaSyDo_zFu75bx_8BAM59z4em99WB7NFgnG6E",
+  "authDomain": "studio-862965224-88487.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "639788228039"
+};
